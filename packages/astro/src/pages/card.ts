5bf83ca35150f23d41d7324d95fb0b3e56fc4583
Dax Ravi <d@ironbay.co>
2023-02-22 15:57:53 -0500

    Add sample cache

--- packages/astro/src/pages/card.ts
+++ packages/astro/src/pages/card.ts
@@ -1,7 +1,9 @@
 import type { APIContext } from "astro"
 import chromium from "chrome-aws-lambda"
-const localChrome =
-  "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
+import os from "os"
+
+const realShit = "/usr/bin/google-chrome-stable"
+const weakShit = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
 
 export async function get(ctx: APIContext) {
   const user = ctx.url.searchParams.get("u")
@@ -18,14 +20,16 @@ export async function get(ctx: APIContext) {
       defaultViewport: chromium.defaultViewport,
       executablePath: chromium.headless
         ? await chromium.executablePath
-        : localChrome,
+        : os.platform() === "linux"
+        ? realShit
+        : weakShit,
       headless: true,
       ignoreHTTPSErrors: true,
     })
 
     const page = await browser.newPage()
     page.setViewport({ width: 1200, height: 630 })
-    await page.goto(ctx.url.origin + "/ticket?" + ctx.url.search)
+    await page.goto(ctx.url.origin + "/ticket" + ctx.url.search)
 
     result = await page.screenshot()
   } catch (error) {
@@ -36,5 +40,9 @@ export async function get(ctx: APIContext) {
     }
   }
 
-  return new Response(result)
+  return new Response(result as any, {
+    headers: {
+      "cache-control": "public, max-age=31536000",
+    },
+  })
 }

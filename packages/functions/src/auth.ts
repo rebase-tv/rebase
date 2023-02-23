e9a235ae2467e787eea92e39d0d2be08d8c46152
Dax Ravi <d@ironbay.co>
2023-02-23 16:24:00 -0500

    Twitter flow

--- packages/functions/src/auth.ts
+++ packages/functions/src/auth.ts
@@ -1,9 +1,12 @@
-import { AuthHandler, GithubAdapter } from "sst/node/auth2"
+import { AuthHandler, GithubAdapter, OauthAdapter } from "sst/node/auth2"
 import { Octokit } from "@octokit/rest"
 import { Config } from "sst/node/config"
 import { User } from "@rebase/core/user"
+import { Issuer } from "openid-client"
 import "sst/node/auth2"
 import { useCookie, useQueryParam, useResponse } from "sst/node/api"
+import { Client as Twitter } from "twitter-api-sdk"
+import { fetch } from "undici"
 
 declare module "sst/node/auth2" {
   export interface SessionTypes {
@@ -23,6 +26,16 @@ export const handler = AuthHandler({
       clientID: Config.GITHUB_CLIENT_ID,
       clientSecret: Config.GITHUB_CLIENT_SECRET,
     }),
+    twitter: OauthAdapter({
+      clientID: "ZkRleTN1Ql9IdHdfQ0h5V2FJUTY6MTpjaQ",
+      clientSecret: "OhcSgbG66noSIvlnLmgNcE6Ix1Zj5d7szvMQy1AHC9NODp0xoS",
+      scope: "users.read tweet.read",
+      issuer: new Issuer({
+        issuer: "https://twitter.com",
+        authorization_endpoint: "https://twitter.com/i/oauth2/authorize",
+        token_endpoint: "https://api.twitter.com/2/oauth2/token",
+      }),
+    }),
   },
   async onAuthorize() {
     const ref = useQueryParam("ref")
@@ -40,7 +53,6 @@ export const handler = AuthHandler({
     let user: User.Info | undefined = undefined
 
     if (input.provider === "github") {
-      console.log(input.tokenset)
       const o = new Octokit({
         auth: input.tokenset.access_token,
       })
@@ -56,6 +68,26 @@ export const handler = AuthHandler({
             name: me.data.name || undefined,
             email: email,
             avatar: me.data.avatar_url,
+            ref: useCookie("ref"),
+          }))
+    }
+
+    if (input.provider === "twitter") {
+      const client = new Twitter(input.tokenset.access_token!)
+      const me = await client.users.findMyUser({
+        "user.fields": ["profile_image_url", "username", "id"],
+      })
+      if (!me.data) throw new Error("No user found")
+      console.log("Twitter user", me)
+      const email = me.data?.id + "+twitter@rebase.tv"
+      const exists = await User.fromEmail(email)
+      exists
+        ? (user = exists)
+        : (user = await User.create({
+            username: me.data!.username,
+            name: me.data.name || undefined,
+            email: email,
+            avatar: me.data.profile_image_url!.replace("_normal", "_400x400"),
             ref: useCookie("ref"),
           }))
     }

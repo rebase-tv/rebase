4be755fb74045d4811f330659aa0bef71cd02074
Dax Ravi <d@ironbay.co>
2023-02-24 10:52:25 -0500

    Fixed exceptions

--- packages/astro/src/data/session.ts
+++ packages/astro/src/data/session.ts
@@ -1,25 +1,25 @@
-import type { AstroCookies } from "astro";
-import { Session, SessionValue } from "sst/node/auth2";
+import type { AstroCookies } from "astro"
+import { Session, SessionValue } from "sst/node/auth2"
 
 declare module "sst/node/auth2" {
   export interface SessionTypes {
     user: {
-      userID: string;
-    };
+      userID: string
+    }
   }
 }
 
 export function useSession(cookies: AstroCookies) {
-  const cookie = cookies.get("session");
-  if (!cookie.value) return;
-  const session = Session.verify(cookie.value);
-  if (!session) return;
-  return session as Extract<SessionValue, { type: "user" }>;
+  const cookie = cookies.get("session")
+  if (!cookie.value) return
+  const session = Session.verify(cookie.value)
+  if (!session) return
+  return session as Extract<SessionValue, { type: "user" }>
 }
 
 export function useUserSession(cookies: AstroCookies) {
-  const session = useSession(cookies);
-  if (!session) throw new Error("Not logged in");
-  if (session.type !== "user") throw new Error("Not a user session");
-  return session;
+  const session = useSession(cookies)
+  if (!session) return
+  if (session.type !== "user") return
+  return session
 }
--- packages/astro/src/pages/api/profile.ts
+++ packages/astro/src/pages/api/profile.ts
@@ -4,7 +4,7 @@ import { User } from "@rebase/core/user"
 
 export async function get(ctx: APIContext) {
   const session = useUserSession(ctx.cookies)
-  if (!session) return {}
+  if (!session) return new Response("Unauthorized", { status: 401 })
 
   const user = await User.fromID(session.properties.userID)
   return new Response(JSON.stringify(user))

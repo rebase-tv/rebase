9f03ca0cae4ea64429e3057b2e6dbf64e1332a51
Dax Raad <d@ironbay.co>
2023-03-09 08:26:26 -0500

    Add trpc back

--- packages/app/App.tsx
+++ packages/app/App.tsx
@@ -16,6 +16,7 @@ import Animated, {
   interpolate,
 } from "react-native-reanimated"
 import MaskedView from "@react-native-masked-view/masked-view"
+import { trpc, TRPCProvider } from "data/trpc"
 
 const StyledIonicons = styled(Ionicons)
 const StyledTouchableHighlight = styled(TouchableHighlight)
@@ -26,27 +27,11 @@ const AnimatedMaskedView = Animated.createAnimatedComponent(StyledMaskedView)
 
 const duration = 333
 
-const getStreamUrl = async () => {
-  const playbackUrl = await fetch(
-    `${Constants.expoConfig.extra?.apiUrl}/stream`
-  )
-  const { streamUrl } = (await playbackUrl.json()) as { streamUrl: string }
-  return streamUrl
-}
-
 export default function App() {
+  const stream = trpc.stream_create.useQuery({})
   const [cue, setCue] = useState<TextMetadataCue>()
   const [qualities, setQualities] = useState<Quality[]>()
-  const [streamUrl, setStreamUrl] = useState<string>()
   const transition = useSharedValue(0)
-
-  useEffect(() => {
-    async function init() {
-      const url = await getStreamUrl()
-      setStreamUrl(url)
-    }
-    init()
-  }, [])
 
   useEffect(() => {
     if (!cue) return
@@ -58,7 +43,7 @@ export default function App() {
     }, 1000 * 10)
   }, [cue, setCue])
 
-  const handlePress = () => { }
+  const handlePress = () => {}
 
   const hostAvatarStyles = useAnimatedStyle(() => {
     const scale = interpolate(transition.value, [0, 1], [3.3, 1])
@@ -102,78 +87,80 @@ export default function App() {
   })
 
   return (
-    <StyledMaskedView
-      className="relative flex-1 flex-row h-full"
-      maskElement={
-        <View
-          style={{
-            // Transparent background because mask is based off alpha channel.
-            backgroundColor: "transparent",
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-        >
-          {/* <Animated.Image */}
-          {/*   source={require("./assets/circle-mask.png")} */}
-          {/*   className="w-full h-full object-cover" */}
-          {/* /> */}
-          <View className="w-full h-full bg-black" />
-        </View>
-      }
-    >
-      <IVSPlayer
-        autoplay
-        streamUrl={streamUrl}
-        onData={(data) => setQualities(data.qualities)}
-        onTextMetadataCue={setCue}
+    <TRPCProvider>
+      <StyledMaskedView
+        className="relative flex-1 flex-row h-full"
+        maskElement={
+          <View
+            style={{
+              // Transparent background because mask is based off alpha channel.
+              backgroundColor: "transparent",
+              flex: 1,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {/* <Animated.Image */}
+            {/*   source={require("./assets/circle-mask.png")} */}
+            {/*   className="w-full h-full object-cover" */}
+            {/* /> */}
+            <View className="w-full h-full bg-black" />
+          </View>
+        }
       >
-        <View className="absolute inset-0" style={{ opacity: cue ? 1 : 0 }}>
-          <View className="absolute inset-0 bg-navy/80" />
-          <Animated.View
-            // entering={SlideInUp.duration(500).easing(Easing.bounce)}
-            className="absolute top-8 inset-x-2 bg-light rounded-2xl h-2/3 flex-1 items-center justify-center"
-          >
-            <View className="absolute top-0 inset-x-0">
-              <AnimatedMaskedView
-                // entering={entering}
-                className="mt-5 w-24 aspect-[1080/1920] mx-auto overflow-clip"
-                style={hostAvatarStyles}
-                maskElement={
-                  <View className="absolute inset-0 bg-transparent flex-1 justify-center items-center">
-                    <Animated.View
-                      // entering={maskEntering}
-                      className="absolute inset-0 bg-black"
-                      style={hostMaskStyles}
-                    />
-                  </View>
-                }
-              >
-                <StyledIVSPlayer
-                  autoplay
-                  streamUrl={streamUrl}
-                  onData={(data) => setQualities(data.qualities)}
-                  onTextMetadataCue={setCue}
-                  className="absolute inset-x-0 aspect-[1080/1920] object-cover bg-pink"
-                />
-              </AnimatedMaskedView>
-            </View>
-          </Animated.View>
-        </View>
-        <View className="absolute bottom-8 right-4">
-          <StyledTouchableHighlight
-            onPress={handlePress}
-            className="bg-pink rounded-full w-12 h-12 flex items-center justify-center"
-          >
-            <StyledIonicons
-              name="md-heart"
-              size={24}
-              color="white"
-              className="ml-0 mt-1"
-            />
-          </StyledTouchableHighlight>
-        </View>
-      </IVSPlayer>
-    </StyledMaskedView>
+        <IVSPlayer
+          autoplay
+          streamUrl={stream.data.url}
+          onData={(data) => setQualities(data.qualities)}
+          onTextMetadataCue={setCue}
+        >
+          <View className="absolute inset-0" style={{ opacity: cue ? 1 : 0 }}>
+            <View className="absolute inset-0 bg-navy/80" />
+            <Animated.View
+              // entering={SlideInUp.duration(500).easing(Easing.bounce)}
+              className="absolute top-8 inset-x-2 bg-light rounded-2xl h-2/3 flex-1 items-center justify-center"
+            >
+              <View className="absolute top-0 inset-x-0">
+                <AnimatedMaskedView
+                  // entering={entering}
+                  className="mt-5 w-24 aspect-[1080/1920] mx-auto overflow-clip"
+                  style={hostAvatarStyles}
+                  maskElement={
+                    <View className="absolute inset-0 bg-transparent flex-1 justify-center items-center">
+                      <Animated.View
+                        // entering={maskEntering}
+                        className="absolute inset-0 bg-black"
+                        style={hostMaskStyles}
+                      />
+                    </View>
+                  }
+                >
+                  <StyledIVSPlayer
+                    autoplay
+                    streamUrl={streamUrl}
+                    onData={(data) => setQualities(data.qualities)}
+                    onTextMetadataCue={setCue}
+                    className="absolute inset-x-0 aspect-[1080/1920] object-cover bg-pink"
+                  />
+                </AnimatedMaskedView>
+              </View>
+            </Animated.View>
+          </View>
+          <View className="absolute bottom-8 right-4">
+            <StyledTouchableHighlight
+              onPress={handlePress}
+              className="bg-pink rounded-full w-12 h-12 flex items-center justify-center"
+            >
+              <StyledIonicons
+                name="md-heart"
+                size={24}
+                color="white"
+                className="ml-0 mt-1"
+              />
+            </StyledTouchableHighlight>
+          </View>
+        </IVSPlayer>
+      </StyledMaskedView>
+    </TRPCProvider>
   )
 }
